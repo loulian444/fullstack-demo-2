9468890eac06a53424b978b774727b64fb197846
Loulian Liu <3l.liu444@gmail.com>
2023-08-21 16:01:50 -0400

    added token to local storage for quality of life. hope i dont get hacked

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,13 +2,17 @@ import AuthForm from "./AuthForm";
 import ShowInfo from "./ShowInfo";
 
 const Home = ({ token, setToken }) => {
+  const clickHandler = () => {
+    localStorage.removeItem("token");
+    setToken(localStorage.getItem("token"));
+  };
 
   return (
     <>
       <h1>REACT TRAIN APP</h1>
       <p>Choo Choo!</p>
 
-      {token ? <button onClick={() => setToken(false)}>Sign Out</button> : ""}
+      {token ? <button onClick={clickHandler}>Sign Out</button> : ""}
 
       {token ? <ShowInfo token={token} /> : <AuthForm setToken={setToken} />}
     </>
--- client/src/components/Router.jsx
+++ client/src/components/Router.jsx
@@ -1,10 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import RegisterForm from "./RegisterForm";
 import Home from "./Home";
 
 const Router = () => {
   const [token, setToken] = useState(null);
+  
+  useEffect(() => {
+    !token ? setToken(localStorage.getItem("token")) : localStorage.setItem("token", token);
+  }, [token])
 
   return (
     <Routes>
